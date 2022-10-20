72d105498c520459848637f8f76fc314725bf802
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-20 22:49:28 +0500

    Index09.py

--- Index09.py
+++ Index09.py
@@ -6,4 +6,27 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s=='0':
+        d=int(s)
+    if s=='1':
+        d=int(s)
+    if s=='2':
+        d=int(s)
+    if s=='3':
+        d=int(s)
+    if s=='4':
+        d=int(s)
+    if s=='5':
+        d=int(s)
+    if s=='6':
+        d=int(s)
+    if s=='7':
+        d=int(s)
+    if s=='8':
+        d=int(s)
+    if s=='9':
+        d=int(s)
+    else:
+        d=-1
+    return d
+print(main('q'))
