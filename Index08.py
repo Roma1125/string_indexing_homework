ab80bbf50d2247dbf4cafe55481f28666676f0e8
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-20 23:04:24 +0500

    Index08.py

--- Index08.py
+++ Index08.py
@@ -7,15 +7,15 @@ def main(s):
         int: answer
     """
     if s[0]=='*':
-        d=0
+        answer=0
     if s[1]=='*':
-        d=1
+        answer=1
     if s[2]=='*':
-        d=2
+        answe=2
     if s[3]=='*':
-        d=3
+        answe=3
     if s[4]=='*':
-        d=4
+        answe=4
     return d
 print(main('asdf*'))
         
