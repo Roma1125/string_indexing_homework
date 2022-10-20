ee09ba7bee1b0afcf94f5ba2ddaed5eb7c1b5933
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-20 23:19:26 +0500

    Index09.py

--- Index09.py
+++ Index09.py
@@ -7,26 +7,26 @@ def main(s):
         int: answer
     """
     if s=='0':
-        d=int(s)
+        answer=int(s)
     if s=='1':
-        d=int(s)
+        answer=int(s)
     if s=='2':
-        d=int(s)
+        answer=int(s)
     if s=='3':
-        d=int(s)
+        answer=int(s)
     if s=='4':
-        d=int(s)
+        answer=int(s)
     if s=='5':
-        d=int(s)
+        answer=int(s)
     if s=='6':
-        d=int(s)
+        answer=int(s)
     if s=='7':
-        d=int(s)
+        answer=int(s)
     if s=='8':
-        d=int(s)
+        answer=int(s)
     if s=='9':
-        d=int(s)
+        answer=int(s)
     else:
-        d=-1
-    return d
+        answer=-1
+    return answer
 print(main('q'))
